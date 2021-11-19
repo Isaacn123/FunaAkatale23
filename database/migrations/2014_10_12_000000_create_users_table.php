d4b630f96ea706fa0da7b3a12257022893276e03
isaac1212 <nsambai722@gmail.com>
2021-11-19 19:02:39 +0300

    phone update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('zipcode')->nullable();
             $table->string('image')->default("noimage.jpg");
             $table->boolean('status')->default(1);
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
