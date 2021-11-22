fd58e6050b89d8df057ab5c1264e4aa20d09d9b6
isaac1212 <nsambai722@gmail.com>
2021-11-22 15:13:45 +0300

    addres

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Address;
 
 class User extends Authenticatable
 {
@@ -59,6 +60,6 @@ class User extends Authenticatable
 
     public function address()
     {
-        return $this->hasMany('App\Address');
+        return $this->hasMany('Address');
     }
 }
