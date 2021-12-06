024a5f9056a7c89c299d13912b551d83152a64b8
Isaacn123 <nsambai72@gmail.com>
2021-12-06 23:42:46 +0300

    database seed for product and review

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,28 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-     
-        $faker = Faker::create();
-
-        foreach(range(0, 30) as $index){
-          DB::table('business')->insert([
-            'name' => $faker->company,
-            'description' => $faker->realText($maxNbChars = 200, $indexSize = 2),
-            'website' => $faker->domainName,
-            'slug' => $faker->slug,
-            'andress' => $faker-> address,
-            'email' => $faker->unique()->safeEmail,
-            'contact' => $faker->phoneNumber ,
-            'categoryName' => 'category',
-            'subcategoryName' => 'safeSubcategory',
-            'country' => $faker-> country,
-            'fax' => $faker->unique()-> tollFreePhoneNumber,
-            'city' => $faker-> city,
-            'image' => $faker -> imageUrl($width = 640, $height = 480),
-            'created_at' => $faker->dateTimeBetween('-6 month','+1 month')
-          ]);
-        }
-
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->count(10) -> create();
+        \App\Models\Product::factory()->count(20) -> create();
+        \App\Models\Review::factory() -> count(200) -> create(); 
     }
 }
